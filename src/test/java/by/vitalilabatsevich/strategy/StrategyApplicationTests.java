db445ac1fb46aedef310bb40889dada4010c36ab
Vitali Labatsevich <vitaly.lobatsevich@gmail.com>
2023-03-05 19:58:18 +0300

    Created all base project files: Maven, Java, Docker, modified .gitignore for new project

---
+++ src/test/java/by/vitalilabatsevich/strategy/StrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package by.vitalilabatsevich.strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
